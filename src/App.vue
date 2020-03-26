3be3b134ec4bdefa2f42fbd51449175a29a561ea
ellison <xuyongnojs@163.com>
2020-03-26 15:26:39 +0800

    xuyong

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="欢迎来到 朋朋 和涌哥的 测试栏"/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import jwt_decode from "jwt-decode";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "app",
+  created() {
+    if (localStorage.eleToken) {
+      const decode = jwt_decode(localStorage.eleToken);
+      this.$store.dispatch("setIsAutnenticated", !this.isEmpty(decode));
+      this.$store.dispatch("setUser", decode);
+    }
+  },
+  methods: {
+    isEmpty(value) {
+      return (
+        value === undefined ||
+        value === null ||
+        (typeof value === "object" && Object.keys(value).length === 0) ||
+        (typeof value === "string" && value.trim().length === 0)
+      );
+    }
   }
-}
+};
 </script>
 
+
 <style>
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
 }
 </style>
